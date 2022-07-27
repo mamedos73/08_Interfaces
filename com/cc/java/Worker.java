9f1127a9adf0a772e59912f481ed875cfe59a550
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-07-27 09:12:21 +0200

    Add files via upload

---
+++ com/cc/java/Worker.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Worker extends HoneyBee{
+   
+    public String doYourJob() {
+        return "I'm a worker bee, I work all day!";
+     }
+
+    @Override
+    public String fly() {
+        return "I believe, I can fly  at 12mph!";
+    }
+
+
+}
