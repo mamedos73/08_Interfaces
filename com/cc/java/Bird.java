0d2d118a6665395fea2bdf64eb12215cae57217d
Roman <roman.makhanbetov@gmx.de>
2022-07-27 10:14:09 +0200

    Bird()

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee {
+
+    @Override
+    public String doYourJob() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
